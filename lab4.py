03605d5c679215837d3b998274270d73a1a55701
aabdullatipova1 <abdullatipova.asm1704@asugubkin.ru>
2017-11-12 18:47:45 +0300

    (+) add st01

--- lab4.py
+++ lab4.py
@@ -1,4 +1,5 @@
 ﻿import st00.main
+import st01.main
 import st22.main
 import st26.main
 import st38.main
@@ -7,6 +8,7 @@ import st38.main
 
 MENU = [
         ["[00] Образец", st00.main.main],
+        ["[01] Абдуллатипова", st01.main.main],
         ["[22] Ишмаметьев", st22.main.main],
         ["[26] Ларионов", st26.main.main],
         ["[38] Солопеева", st38.main.main],
