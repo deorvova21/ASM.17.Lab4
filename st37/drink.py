84dc24b86ad179174851140a474666d02eba9eb6
stiks321 <31651628+stiks321@users.noreply.github.com>
2017-11-28 13:03:49 +0300

    Add files via upload

--- st37/drink.py
+++ st37/drink.py
@@ -10,5 +10,5 @@ class drink(food):
 
 
     def peredacha(self, idd):
-        urllib.request.urlopen("http://localhost:81/cgi-bin/lab3.py?index=-1&student=" + idd +"&name="+urq.quote(self.name)+"&quantity="+urq.quote(self.quantity)+"&calories="+urq.quote(self.calories)+"&alcogol="+urq.quote(self.alcogol))
+        urllib.request.urlopen("http://localhost:81/cgi-bin/lab3.py?index=-1&student=" + idd +"&name="+urq.quote(self.name)+"&quantity="+urq.quote(self.quantity)+"&calories="+urq.quote(self.calories)+"&alcogol="+urq.quote(self.alcogol)+"&j=okok")
         
--- st37/food.py
+++ st37/food.py
@@ -10,6 +10,6 @@ class food:
 
 
     def peredacha(self, idd):
-        urllib.request.urlopen("http://localhost:81/cgi-bin/lab3.py?index=-1&student=" + idd + "&name="+urllib.quote(self.name)+"&quantity="+urllib.quote(self.quantity)+"&calories="+urllib.quote(self.calories))
+        urllib.request.urlopen("http://localhost:81/cgi-bin/lab3.py?index=-1&student=" + idd + "&name="+urllib.quote(self.name)+"&quantity="+urllib.quote(self.quantity)+"&calories="+urllib.quote(self.calories)+"&j=ok")
         
     
