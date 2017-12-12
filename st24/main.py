78f7caa8ca98db561c274bcc0ff52d0d191ca834
dvoryanchikov <dvoryanchikov.asm1604@asugubkin.ru>
2017-12-12 13:35:51 +0300

    rwetywe

--- st24/main.py
+++ st24/main.py
@@ -10,26 +10,25 @@ PATH = 'st24/uploading_data.pkl'
 
 
 def main():
-    data = None
+    global data
     with open(PATH, 'rb') as f:
-        global data
         data = pickle.load(f)
 
     response = rq.urlopen('http://localhost:81/cgi-bin/lab3.py')
     html_data = str(response.read())
 
-    for item in data:
+    for item in data[0]:
         params = {
-            'fullname': data._name,
-            'party_name': data._party_name,
-            'id': data.id,
+            'fullname': item._name,
+            'party_name': item._party_name,
+            'id': item.id,
             'student':re.search(r'student=(\d+)\">\[' + str(MY_NUMBER) + '\]', html_data).group(1)
         }
         if (type(item) is Comrade):
             params['action'] = 'add_comrade'
         else:
             params['action'] = 'add_party_elite'
-            params['position'] =  data._position
+            params['position'] =  item._position
         request_url = 'http://localhost:81/cgi-bin/lab3.py' + '?' + prs.urlencode(params)
         rq.urlopen(request_url)
 
